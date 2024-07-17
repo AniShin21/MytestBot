a01059835f3c4329c7f237050a93cd36f3860897
AniShin21 <minecraft2727k@gmail.com>
2024-07-17 17:19:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,17 +17,17 @@ print(f'Public IP Address: {ip}')
 load_dotenv()
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-APP_ID = int(os.environ.get("APP_ID", "28277294"))
-API_HASH = os.environ.get("API_HASH", "7ec1420b010830ad701cf518ce0474580")
+APP_ID = int(os.environ.get("APP_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
 
 
-OWNER = os.environ.get("OWNER", "@ULTIMATE_GUARDIAN_X")  # Owner username
-OWNER_ID = int(os.environ.get("OWNER_ID", "1768198143"))  # Owner user id
+OWNER = os.environ.get("OWNER", "")  # Owner username
+OWNER_ID = int(os.environ.get("OWNER_ID", ""))  # Owner user id
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002122561727"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 FORCE_SUB_CHANNEL1 = int(os.environ.get(
     "FORCE_SUB_CHANNEL1", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get(
@@ -47,7 +47,7 @@ START_MSG = os.environ.get(
 
 try:
     ADMINS = [1768198143]
-    for x in (os.environ.get("ADMINS", "1768198143 6450266465 7065803173").split()):
+    for x in (os.environ.get("ADMINS", "").split()):
         ADMINS.append(int(x))
 except ValueError:
     raise Exception("Your Admins list does not contain valid integers.")
