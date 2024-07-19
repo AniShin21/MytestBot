ee742fac8e387eba53041d5ab43415b25a3b2006
AniShin21 <minecraft2727k@gmail.com>
2024-07-19 20:33:01 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -35,8 +35,38 @@ async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
 
+#admins
 
-async def remove_ban(self, id):
+async def present_admin(user_id: int):
+    found = await admin_data.find_one({'_id': user_id})
+    return bool(found)
+
+
+async def add_admin(user_id: int):
+    user = new_user(user_id)
+    await admin_data.insert_one(user)
+    ADMINS.append(int(user_id))
+    return
+
+async def del_admin(user_id: int):
+    await admin_data.delete_one({'_id': user_id})
+    ADMINS.remove(int(user_id))
+    return
+
+async def full_adminbase():
+    user_docs = admin_data.find()
+    user_ids = [int(doc['_id']) async for doc in user_docs]
+    return user_ids
+
+
+async def get_all_users(self):
+        all_users = self.col.find({})
+        return all_users
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def remove_ban(self, id):
         ban_status = dict(
             is_banned=False,
             ban_duration=0,
@@ -70,7 +100,6 @@ async def remove_ban(self, id):
 
 
 db = Database(Config.DATABASE_URL, Config.BOT_USERNAME)
-
 
 #admins
 
